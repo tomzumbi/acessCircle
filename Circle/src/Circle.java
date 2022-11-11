76094d684bdfa5e0792621c7c8389a78775cfb17
tomzumbi <114890330+tomzumbi@users.noreply.github.com>
2022-11-11 13:39:56 +0700

    Add files via upload

---
+++ Circle/src/Circle.java
@@ -0,0 +1,20 @@
+public class Circle {
+    private double radius;
+    private String color = "red";
+
+    public Circle() {
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public String getColor() {
+        return color = "red";
+    }
+}
+
